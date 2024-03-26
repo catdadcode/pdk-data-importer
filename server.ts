efe70ee78d38942d7f015a6975d9f78d28bf37f0
Alex Ford <Alex@CatDadCode.dev>
2024-03-26 10:04:55 -0600

    Add comments to the code.

--- server.ts
+++ server.ts
@@ -1,3 +1,4 @@
+// Pull in dotenv package to load environment variables from a .env file
 import "dotenv/config";
 import debug from "debug";
 import express from "express";
@@ -7,14 +8,18 @@ import fs from "fs/promises";
 import path from "path";
 import { Worker } from "worker_threads";
 import { v4 as uuidv4 } from "uuid";
+// Import the types we defined in the types.ts file to keep them organized.
 import type {
   ClientActions,
   FileData,
   ServerActions
 } from "./types";
 
+// We use the debug module to print more detailed logs that are easy to disable in production.
 const log = debug("pdk:server");
 
+// Ensure that the uploads/ directory exists to avoid errors when saving files.
+// You'll also notice we are using top-level await in this project :D
 try {
   await fs.access(path.join(import.meta.dirname, "uploads"));
 } catch (err) {
@@ -22,24 +27,29 @@ try {
 }
 
 const app = express();
+// We need to create our own server instance so we can attach both express and the WebSocket server to it.
 const server = createServer(app);
 
 // Initialize WebSocket server
 const wss = new WebSocketServer({ server });
 
 wss.on("connection", (ws) => {
+	// Remember the file name passed in via the metadata payload.
+	// This need only be remembered for the subsequent binary message over the socket.
+	// The client code ensures these messages arrive in the correct order regardless of file read time.
   let lastFileName: string;
 	ws.on("message", async (message) => {
     let actionData: ServerActions | undefined;
     let buffer: Buffer | undefined;
+		// Try to parse the payload as a JSON string and fall back to a Buffer if it fails.
     try {
       actionData = JSON.parse(message.toString()) as ServerActions;
     } catch {
       buffer = message as Buffer;
     }
 
+		// If it was a JSON message then it will contain an action and arguments.
 		if (actionData) {
-			// Handle JSON messages
 			const { action } = actionData;
       switch (action) {
         case "INIT_UPLOAD":
@@ -58,11 +68,13 @@ wss.on("connection", (ws) => {
         // Add more actions here
       }
 		} else if (buffer) {
-			// Handle binary data (the file itself)
+			// Handle binary data for the file itself
       const fileName = lastFileName;
-			const uniqueName = uuidv4(); // Generate a unique filename
+			// Generate a unique filename so users don't overwrite each other's files in temporary upload storage.
+			const uniqueName = uuidv4(); 
 
 			try {
+				// Write the file to temporary storage so it can be retrieved later by the worker thread.
 				await fs.writeFile(path.join(import.meta.dirname, "uploads", uniqueName), buffer);
 				log(`File received and saved: ${uniqueName}`);
 
@@ -101,16 +113,18 @@ wss.on("connection", (ws) => {
 				});
 
 				worker.on("exit", (code) => {
-					if (code !== 0)
+					if (code !== 0) {
 						console.error(`Worker stopped with exit code ${code}`);
+						// TODO: cleanup temporary files
+					}
 				});
 			} catch (err: any) {
 				console.error("Failed to save the file:", err);
 				ws.send(
 					JSON.stringify({
-						action: "ERROR",
+						action: "STATUS_UPDATE",
 						fileName,
-						error: err?.message || err,
+						status: err?.message || err,
 					}),
 				);
 			}
